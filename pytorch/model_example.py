2cae1885f590a3fe18d6263fe9c4dd3a21bdf591
chenguangzeng <chenguangzeng@cgz.com>
2025-12-29 14:07:58 +0800

    feat: add pytorch

--- pytorch/model_example.py
+++ pytorch/model_example.py
@@ -18,12 +18,26 @@ model = nn.Sequential(
    nn.Sigmoid()           # 输出层的Sigmoid激活函数
 )
 
+# 等价的模型定义方式
+# class SimpleNN(nn.Module):
+#     def __init__(self):
+#         super(SimpleNN, self).__init__()
+#         self.fc1 = nn.Linear(n_in, n_h)
+#         self.relu = nn.ReLU()
+#         self.fc2 = nn.Linear(n_h, n_out)
+#         self.sigmoid = nn.Sigmoid()
+#     
+#     def forward(self, x):
+#         x = self.relu(self.fc1(x))
+#         x = self.sigmoid(self.fc2(x))
+#         return x
+
 # 定义均方误差损失函数和随机梯度下降优化器
 criterion = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)  # 学习率为0.01
 
 # 执行梯度下降算法进行模型训练
-for epoch in range(50):  # 迭代50次
+for epoch in range(100):  # 迭代50次
    y_pred = model(x)  # 前向传播，计算预测值
    loss = criterion(y_pred, y)  # 计算损失
    print('epoch: ', epoch, 'loss: ', loss.item())  # 打印损失值
@@ -33,4 +47,7 @@ for epoch in range(50):  # 迭代50次
    optimizer.step()  # 更新模型参数
    
 print("y_pred after training:", model(x).detach().numpy())
+# 预测结果是概率，输出大于0.5的为1，否则为0
+y_result = (model(x)[:, 0] > 0.5).float().unsqueeze(1)
+print("y result after training:", y_result.numpy())
 print("y true:", y.numpy())
