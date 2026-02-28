fa59402a598e551678478c239a16a0b2f868db01
chenguangzeng <chenguangzeng@cgz.com>
2026-02-28 15:28:20 +0800

    feat: add

--- pytorch/onnx2tensor.py
+++ pytorch/onnx2tensor.py
@@ -9,6 +9,11 @@ network = builder.create_network(1 << int(trt.NetworkDefinitionCreationFlag.EXPL
 parser  = trt.OnnxParser(network, logger)
 config  = builder.create_builder_config()
 
+# batch是动态的，所以需要设置优化配置文件
+profile = builder.create_optimization_profile()
+
+# 解析ONNX模型
+print("Parsing ONNX model...")
 with open(onnx_path, "rb") as f:
     parsed = parser.parse(f.read())
 
@@ -17,6 +22,14 @@ if not parsed:
     for i in range(parser.num_errors):
         print(parser.get_error(i))
     raise RuntimeError("ONNX parse failed")
+print("ONNX model parsed successfully.")
+
+inp = network.get_input(0)
+print(f"Input name: {inp.name}, shape: {inp.shape}, dtype: {inp.dtype}")
+input_name = network.get_input(0).name
+print(f"Input name: {input_name}")
+profile.set_shape(input_name, min=(1,10), opt=(10,10), max=(100,10))
+config.add_optimization_profile(profile)
 
 engineString = builder.build_serialized_network(network, config)
 
